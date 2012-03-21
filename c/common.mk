fa9f4604cff438d793cd35df5e220b971ae92b6f
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2012-03-21 18:46:11 +0100

    Added automake structure

---
+++ c/common.mk
@@ -0,0 +1,27 @@
+## @file common.mk
+## @author Tim van Werkhoven (werkhoven@strw.leidenuniv.nl)
+## Common makefile directives for all subdirs.
+
+### Inclusion directories
+
+LIB_DIR = $(top_builddir)/unwrap
+
+### Common flags
+
+AM_CPPFLAGS = -I$(LIB_DIR)
+#				$(COMMON_CFLAGS)
+
+AM_CPPFLAGS += -D__STDC_FORMAT_MACROS \
+		-D__STDC_LIMIT_MACROS
+    
+#LDADD = $(COMMON_LIBS)
+
+# More error reporting during compilation
+AM_CFLAGS = -Wall -Wextra -Wfatal-errors
+
+### Debug options
+if HAVE_DEBUG
+AM_CFLAGS += -ggdb -g3 -O0 -fno-inline
+else
+AM_CFLAGS += -O3 -ftree-vectorize
+endif
