1e9782be61c7ff4923eb39bd1137a8fc179d9ae2
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2012-03-21 15:05:38 +0100

    Add sanity test, add unittest

--- python/unwrap/unwrap.py
+++ python/unwrap/unwrap.py
@@ -18,10 +18,11 @@ This is unwrap.py, a Python wrapper for phase unwrapping routines.
 
 # Importe C routines
 import unwrap_c
-# Use Numpy
-import numpy as N
 
-## @brief Pure Python wrapper for C-Python library
+import numpy as N
+import unittest
+
+## @brief Unwrap phase, wrapper for C-function
 #
 # This is a Python wrapper for the Python-C library. It mostly checks and
 # sanitizes the parameters before passing it on to the Python-C library.
@@ -29,7 +30,7 @@ import numpy as N
 # Available methods include:
 # - Itoh
 # - Floodfill
-def unwrap(phase, method="itoh"):
+def unwrap(phase, method="itoh", *args):
 	print "unwrap(...)"
 
 	# Check method validity
@@ -37,12 +38,57 @@ def unwrap(phase, method="itoh"):
 	if (method.lower() not in val_meths):
 		raise ValueError("Invalid method, must be one of " + str(val_meths))
 
-	# Check phase dimension
+	# Check sanity of phase
+	__check_sanity(phase)
 
-	# Check phase datatype
-
-	# Wrap to C-library
+	# Check method and call function
+	if (method == 'flood'):
+		return unwrap_c.floodfill(phase, args)
+	elif (method == 'itoh'):
+		return
 
 	# Return results
 
 	return
+
+## @brief Check if phase seems ok
+#
+def __check_sanity(phase, ndim=2, dtlist=[N.float]):
+	if (phase.ndim != ndim):
+		raise ValueError("Phase should be %d-dimensional." % (ndim))
+	if (phase.dtype not in dtlist):
+		raise ValueError("Phase should be on of %s." % str(dtlist))
+
+### Test routines ###########################################################
+
+class TestSanityCheck(unittest.TestCase):
+	def setUp(self):
+		"""Generate fake phase"""
+		self.sz = (257, 509)
+		grid = N.indices(self.sz) / N.r_[self.sz].reshape(-1,1,1)
+		# Random phase
+		self.phase = N.sin(grid[0] * N.pi * 2)*3. + N.cos(grid[1] * N.pi * 8)*4.
+		# Wrapped phase
+		self.phase_wr = (self.phase % (2*N.pi)) - N.pi
+
+	# Shallow function tests
+	def test0b_sanity(self):
+		"""Test __check_sanity() deeper"""
+		self.assertRaisesRegexp(ValueError, 'Phase should be 2-dimensional.',
+                        unwrap, N.arange(100.0))
+		self.assertRaisesRegexp(ValueError, 'Phase should be on of.*',
+                        unwrap, N.arange(100).reshape(10,10))
+
+	def test0c_unwrap(self):
+		"""Test unwrap methods"""
+		self.assertRaisesRegexp(ValueError, 'Invalid method.*',
+                        unwrap, N.arange(100.0), 'invalid')
+
+	def test2a(self):
+		"""Test unwrap methods"""
+		test_uw = unwrap(self.phase)
+		self.
+
+# This must be the final part of the file, code after this won't be executed
+if __name__ == "__main__":
+	unittest.main()
