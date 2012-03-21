dd2f6e8d32194599b22434429e3d90f6b34dffa7
Tim van Werkhoven <timvanwerkhoven@gmail.com>
2012-03-21 17:29:32 +0100

    Remove general wrapper function, added tests

--- python/unwrap/unwrap.py
+++ python/unwrap/unwrap.py
@@ -22,80 +22,84 @@ import unwrap_c
 import numpy as N
 import unittest
 
-## @brief Unwrap phase, wrapper for C-function
+## @brief Quality-guided floodfill phase unwrapping
 #
-# This is a Python wrapper for the Python-C library. It mostly checks and
-# sanitizes the parameters before passing it on to the Python-C library.
-#
-# Available methods include:
-# - Itoh
-# - Floodfill
-def unwrap(phase, method="itoh", *args):
-	# Check method validity
-	val_meths = ['itoh', 'flood']
-	if (method.lower() not in val_meths):
-		raise ValueError("Invalid method, must be one of " + str(val_meths))
-
-	# Check sanity of phase
+def floodfill(phase, quality):
+	# Check sanity of phase & quality
 	__check_sanity(phase)
+	__check_sanity(quality)
 
-	# Check method and call function
-	if (method == 'flood'):
-		return floodfill(phase, *args)
-	elif (method == 'itoh'):
-		return phase
-	else:
-		return phase
+	return unwrap_c.floodfill(phase, quality)
 
-## @brief Floodfill phase unwrapping
+## @brief Check if a map seems ok
 #
-def floodfill(phase, startx=0, starty=0):
-	# Check sanity of phase
-	__check_sanity(phase)
-
-	return unwrap_c.floodfill(phase, startx, starty)
-
-## @brief Check if phase seems ok
-#
-def __check_sanity(phase, ndim=2, dtlist=[N.float]):
-	if (phase.ndim != ndim):
-		raise ValueError("Phase should be %d-dimensional." % (ndim))
-	if (phase.dtype not in dtlist):
-		raise ValueError("Phase should be on of %s." % str(dtlist))
+def __check_sanity(data, ndim=2, dtlist=[N.float]):
+	if (data.ndim != ndim):
+		raise ValueError("Data should be %d-dimensional." % (ndim))
+	if (data.dtype not in dtlist):
+		raise ValueError("Data should be on of %s." % str(dtlist))
 
 ### Test routines ###########################################################
 
 class TestSanityCheck(unittest.TestCase):
 	def setUp(self):
 		"""Generate fake phase"""
-		self.sz = (257, 509)
-		self.val_meths = ['itoh', 'flood']
-		grid = N.indices(self.sz) / N.r_[self.sz].reshape(-1,1,1)
+		self.sz = (257, 257)
+		grid = N.indices(self.sz, dtype=N.float) / N.r_[self.sz].reshape(-1,1,1)
 		# Random phase
 		self.phase = N.sin(grid[0] * N.pi * 2)*3. + N.cos(grid[1] * N.pi * 8)*4.
 		# Wrapped phase
 		self.phase_wr = (self.phase % (2*N.pi)) - N.pi
+		# Quality map
+		self.qualmap = 1.0*grid.sum(0)
 
 	# Shallow function tests
+	def test0a_inspect(self):
+		"""Inspect data"""
+# 		plt.figure()
+# 		plt.imshow(self.phase)
+# 		plt.figure()
+# 		plt.imshow(self.phase_wr)
+# 		raw_input()
+		pass
+
 	def test0b_sanity(self):
 		"""Test __check_sanity()"""
-		self.assertRaisesRegexp(ValueError, 'Phase should be 2-dimensional.',
-                        unwrap, N.arange(100.0))
-		self.assertRaisesRegexp(ValueError, 'Phase should be on of.*',
-                        unwrap, N.arange(100).reshape(10,10))
+		self.assertRaisesRegexp(ValueError, '.*should be 2-dimensional.',
+                        floodfill, N.arange(100.0), N.arange(100.0))
+		self.assertRaisesRegexp(ValueError, '.*should be on of.*',
+                        floodfill, N.arange(100).reshape(10,10), N.arange(100).reshape(10,10))
 
-	def test0c_unwrap(self):
-		"""Test unwrap methods check"""
-		self.assertRaisesRegexp(ValueError, 'Invalid method.*',
-                        unwrap, N.arange(100.0), 'invalid')
-
-	def test1a(self):
+	def test1a_flood_dummy(self):
 		"""Testing unwrapped unwrapping (dummy run)"""
-		for m in self.val_meths:
-			test_uw = unwrap(self.phase, method=m)
-			self.assertTrue(N.allclose(test_uw, self.phase))
+		test_uw = floodfill(self.phase, self.qualmap)
+		self.assertAlmostEqual(test_uw.sum(), self.phase.sum())
+
+	def test2a_flood_qual(self):
+		"""Testing quality guided floodfill unwrapping"""
+		test_uw = floodfill(self.phase_wr, self.qualmap)
+		if (not N.allclose(test_uw, self.phase)):
+			print self.phase
+			plt.figure()
+			plt.title("Original phase")
+			plt.imshow(self.phase)
+			plt.colorbar()
+
+			plt.figure()
+			plt.title("Wrapped phase")
+			plt.imshow(self.phase_wr)
+			plt.colorbar()
+
+			plt.figure()
+			plt.title("Recovered phase")
+			plt.imshow(test_uw)
+			plt.colorbar()
+			raw_input()
+
+		self.assertAlmostEqual((test_uw - self.phase).sum(), 0)
 
 
 # This must be the final part of the file, code after this won't be executed
 if __name__ == "__main__":
+	import pylab as plt
 	unittest.main()
